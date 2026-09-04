72e069fa3957412b0e5278a3c467f0dea256715d
Paul Romeo <13206466+p-romeo@users.noreply.github.com>
2026-09-04 11:40:29 -0700

    Sync vite config

--- vite.config.ts
+++ vite.config.ts
@@ -3,14 +3,6 @@ import { defineConfig } from "vite";
 export default defineConfig({
   build: {
     outDir: "dist",
-    sourcemap: true,
-  },
-  server: {
-    port: 4317,
-    strictPort: true,
-  },
-  preview: {
-    port: 4317,
-    strictPort: true,
+    sourcemap: false,
   },
 });
